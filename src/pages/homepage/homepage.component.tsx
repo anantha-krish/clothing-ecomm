943b6e1cd8f39c3c3770c519b3f2f4bb01ca629c
Anantha Krishnan <anantha_krishnan@outlook.com>
2021-07-10 20:16:26 +0530

    add welcome page

--- src/pages/homepage/homepage.component.tsx
+++ src/pages/homepage/homepage.component.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Directory from "../../components/directory/directory.component";
+import Welcome from "../welcome/welcome";
 
 import { HomepageContainer } from "./homepage.styles";
 interface Props {}
 
 const Homepage = (props: Props) => {
+  const [hideHomePage, sethideHomePage] = useState(true);
   return (
-    <HomepageContainer>
-      <Directory />
-    </HomepageContainer>
+    <>
+      {hideHomePage ? (
+        <Welcome onClick={() => sethideHomePage(!hideHomePage)} />
+      ) : (
+        <HomepageContainer>
+          <Directory />
+        </HomepageContainer>
+      )}
+    </>
   );
 };
 
