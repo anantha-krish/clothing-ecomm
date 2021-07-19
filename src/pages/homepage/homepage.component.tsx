422e1e2efa8411992a7b319688958d95d2799792
Anantha Krishnan <anantha_krishnan@outlook.com>
2021-07-19 20:10:50 +0530

    remove video

--- src/pages/homepage/homepage.component.tsx
+++ src/pages/homepage/homepage.component.tsx
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
 import Directory from "../../components/directory/directory.component";
-import Welcome from "../welcome/welcome";
 
 import { HomepageContainer } from "./homepage.styles";
 interface Props {}
 
 const Homepage = (props: Props) => {
-  const [hideHomePage, sethideHomePage] = useState(true);
   return (
-    <>
-      {hideHomePage ? (
-        <Welcome onClick={() => sethideHomePage(!hideHomePage)} />
-      ) : (
-        <HomepageContainer>
-          <Directory />
-        </HomepageContainer>
-      )}
-    </>
+    <HomepageContainer>
+      <Directory />
+    </HomepageContainer>
   );
 };
 
